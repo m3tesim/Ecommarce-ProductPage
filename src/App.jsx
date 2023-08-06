03a796139488d01e6f8090fddde9319055226c41
Tasim <tasim@pop-os.localdomain>
2023-08-06 07:11:55 +0300

    change react router to v5

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
 import { CartItemProvider } from "./components/CartItemsProvider";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Intro from "./pages/intro";
+
 class App extends Component {
   render() {
     return (
       <>
         <Router>
-          <Routes>
-            <Route path="/" element={<Intro />}></Route>
-            <Route
-              path="/product"
-              element={
-                <CartItemProvider>
-                  <NavBar />
-                  <ProductPage />
-                </CartItemProvider>
-              }
-            ></Route>
-          </Routes>
+          <Switch>
+            <Route exact path="/">
+              <Intro />
+            </Route>
+            <Route exact path="/product">
+              <CartItemProvider>
+                <NavBar />
+                <ProductPage />
+              </CartItemProvider>
+            </Route>
+          </Switch>
         </Router>
       </>
     );
--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-export default function Intro() {
-  return (
-    <div className="intro-container">
-      <div className="logo-intro">
-        <img src="/images/logo.png" alt="LOGO" />
-      </div>
-      <div class="container">
-        <div class="progress2 progress-moved">
-          <div class="progress-bar2"></div>
+import React, { Component } from "react";
+
+export default class intro extends Component {
+  handleRoute = () => {
+    // Access the history object from props
+    window.location.assign("/product");
+  };
+
+  render() {
+    setTimeout(this.handleRoute, 2000);
+
+    return (
+      <div className="intro-container">
+        <div className="logo-intro">
+          <img src="/images/logo.png" alt="LOGO" />
+        </div>
+        <div class="bar-container">
+          <div class="progress2 progress-moved">
+            <div class="progress-bar2"></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
