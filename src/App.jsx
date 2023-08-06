85c9aa99a96f04209f2b6a8a34e8ec0f40fa58a4
Tasim <tasim@pop-os.localdomain>
2023-08-06 05:53:14 +0300

    use react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
 import { CartItemProvider } from "./components/CartItemsProvider";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Intro from "./pages/intro";
 class App extends Component {
   render() {
     return (
       <>
-        <CartItemProvider>
-          <NavBar />
-          <ProductPage />
-        </CartItemProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Intro />}></Route>
+            <Route
+              path="/product"
+              element={
+                <CartItemProvider>
+                  <NavBar />
+                  <ProductPage />
+                </CartItemProvider>
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </>
     );
   }
