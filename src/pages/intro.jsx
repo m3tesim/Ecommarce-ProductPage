bc5c69722b53ecf22f5b9a8549fb129af1c369c6
Tasim <tasim@pop-os.localdomain>
2023-08-06 06:36:57 +0300

    add progress bar

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
 export default function Intro() {
-  return <div>intro</div>;
+  return (
+    <div className="intro-container">
+      <div className="logo-intro">
+        <img src="/images/logo.png" alt="LOGO" />
+      </div>
+      <div class="container">
+        <div class="progress2 progress-moved">
+          <div class="progress-bar2"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
